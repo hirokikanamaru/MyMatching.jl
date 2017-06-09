85c5deda3af8460c004bb5682b05443192477a61
HIROKIKANAMARU\Hiroki <hk.616.ka@gmail.com>
2017-06-09 17:26:08 +0900

    break元に戻す

--- src/MyMatching.jl
+++ src/MyMatching.jl
@@ -31,6 +31,12 @@ end
 for j in 1:n
 #男性の選好リストの１巡目、2巡目…
     for i in 1:m
+        #各男性が順に動く
+        if n>=m&&all(m_matched.!=0)
+        #もし女の方が多く、男が全員マッチしていたら次の人に入らずmatchedを返す
+            break
+            return m_matched, f_matched
+        else
             if j<=length(m_prefs[i])&&m_matched[i]==0
             #ある男性がj巡目に、まだ選好を持ち、相手がいない
                 like=m_prefs[i][j]
@@ -50,6 +56,7 @@ for j in 1:n
                     end
                 end
             end
+        end
     end
 end
 
